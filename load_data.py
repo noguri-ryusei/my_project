7f515d0a1e64daeed1810be0051f4fbbf99ab9fd
STARK <56547520+noguri-ryusei@users.noreply.github.com>
2025-03-26 10:42:50 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,10 +1,17 @@
-import pandas as pd
+# 1. ライブラリのインポート
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 
-# データの読み込み
-df = pd.read_csv("data/health_fitness_dataset.csv")
+# ランダムフォレスト
+from sklearn.ensemble import RandomForestClassifier
+# ロジスティック回帰
+from sklearn.linear_model import LogisticRegression
+# 決定木分析
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-# データの確認
-print(df.head())
-
-# 必要に応じてCSVとして保存
-df.to_csv("data/processed_data.csv", index=False)
