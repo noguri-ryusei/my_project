9a278ab95e3bb0e52c588540023301c09e13561f
STARK <56547520+noguri-ryusei@users.noreply.github.com>
2025-03-31 01:46:08 +0900

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,4 +1,11 @@
-import pandas as pd
+# 1. ライブラリのインポート
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
 
 # データの読み込み
 df = pd.read_csv("data/health_fitness_dataset.csv")
@@ -8,3 +15,31 @@ print(df.head())
 
 # 必要に応じてCSVとして保存
 df.to_csv("data/processed_data.csv", index=False)
+
+# メッツ表をもとに分類
+print(df['activity_type'].unique())
+# 置き換え
+activity_mapping = {'Walking': 1,'Tennis': 1,'Yoga': 1,'Dancing': 1, 'Weight Training': 2,'Swimming': 2,'Basketball': 2, 'Cycling': 3,'Running': 3,'HIIT': 3}
+df['activity_type'] = df['activity_type'].map(activity_mapping)
+
+# Hypertension（高血圧）をターゲット（1）、それ以外を（0）に変換
+df['hypertension'] = (df['health_condition'] == 'Hypertension').astype(int)
+# 元の health_condition は不要になるため削除
+df = df.drop(columns=['health_condition'])
+
+# 不要なカラムの削除
+df = df.drop(columns=['participant_id', 'date'])
+
+# カテゴリ変数のエンコーディング
+cat_cols = ['gender', 'intensity']
+encoder = LabelEncoder()
+for col in cat_cols:
+    df[col] = encoder.fit_transform(df[col])
+
+# 三分位でカテゴリに変換（1: 低, 2: 中, 3: 高）
+columns_to_transform = ['height_cm', 'weight_kg', 'calories_burned', 'hours_sleep', 'hydration_level', 'bmi', 'resting_heart_rate', 
+                        'blood_pressure_systolic','blood_pressure_diastolic','fitness_level', 'daily_steps','avg_heart_rate', 'duration_minutes']
+
+# すべてのカラムに対してqcutを適用
+for col in columns_to_transform:
+    df[col] = pd.qcut(df[col], q=3, labels=[1, 2, 3])
